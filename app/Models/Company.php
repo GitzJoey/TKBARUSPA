1c6524487d88a451060df8d63024aed3f60ac53b
GitzJoey <gitzjoey@live.com>
2018-03-26 01:22:21 +0700

    company

---
+++ app/Models/Company.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sugito
+ * Date: 9/7/2016
+ * Time: 9:46 AM
+ */
+
+namespace App\Models;
+
+use Auth;
+use Config;
+use Vinkla\Hashids\Facades\Hashids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $table = 'companies';
+
+    protected $fillable = [
+        'name',
+        'address',
+        'latitude',
+        'longitude',
+        'phone_num',
+        'fax_num',
+        'tax_id',
+        'status',
+        'is_default',
+        'frontweb',
+        'image_filename',
+        'remarks',
+        'date_format',
+        'time_format',
+        'thousand_separator',
+        'decimal_separator',
+        'decimal_digit',
+        'ribbon'
+    ];
+
+    protected $hidden = [
+        'created_by',
+        'created_at',
+        'updated_by',
+        'updated_at',
+        'deleted_by',
+        'deleted_at',
+    ];
+
+    protected $appends = [
+        'numeralFormat',
+        'dateFormat',
+        'timeFormat',
+        'dateTimeformat'
+    ];
+
+    public function getNumeralFormatAttribute()
+    {
+        $thousandSeparator = is_null($this->attributes['thousand_separator']) ? ',':$this->attributes['thousand_separator'];
+        $decimalSeparator = is_null($this->attributes['decimal_separator']) ? '.':$this->attributes['decimal_separator'];
+        $decimalDigit = '';
+
+        if ($this->attributes['decimal_digit'] == 0) {
+            $decimalDigit = '00';
+        } else {
+            for ($i = 0; $i < $this->attributes['decimal_digit']; $i++) {
+                $decimalDigit .= '0';
+            }
+        }
+
+        return '0'.$thousandSeparator.'0'.'['.$decimalSeparator.']'.$decimalDigit;
+    }
+
+    public function getDateFormatAttribute()
+    {
+        if (is_null($this->attributes['date_format']) || empty($this->attributes['date_format'])) {
+            return Config::get('const.DATETIME_FORMAT.PHP_DATE');
+        } else {
+            return $this->attributes['date_format'];
+        }
+    }
+
+    public function getTimeFormatAttribute()
+    {
+        if (is_null($this->attributes['time_format']) || empty($this->attributes['time_format'])) {
+            return Config::get('const.DATETIME_FORMAT.PHP_TIME');
+        } else {
+            return $this->attributes['time_format'];
+        }
+    }
+
+    public function getDateTimeFormatAttribute()
+    {
+        return $this->getDateFormatAttribute() . ' ' . $this->getTimeFormatAttribute();
+    }
+
+    public function hId()
+    {
+        return HashIds::encode($this->attributes['id']);
+    }
+
+    public function users()
+    {
+        return $this->hasMany('App\User');
+    }
+
+    public function products()
+    {
+        return $this->hasMany('App\Models\Product');
+    }
+
+    public function purchaseOrders()
+    {
+        return $this->hasMany('App\Models\PurchaseOrder');
+    }
+
+    public function bankAccounts()
+    {
+        return $this->morphMany('App\Models\BankAccount', 'owner');
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $user = Auth::user();
+            if ($user) {
+                $model->created_by = $user->id;
+                $model->updated_by = $user->id;
+            }
+        });
+
+        static::updating(function ($model) {
+            $user = Auth::user();
+            if ($user) {
+                $model->updated_by = $user->id;
+            }
+        });
+
+        static::deleting(function ($model) {
+            $user = Auth::user();
+            if ($user) {
+                $model->deleted_by = $user->id;
+                $model->save();
+            }
+        });
+    }
+}
---
+++ database/migrations/2016_09_07_000001_create_companies_table.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sugito
+ * Date: 9/7/2016
+ * Time: 11:06 AM
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('address')->nullable();
+            $table->decimal('latitude', 9, 6)->nullable();
+            $table->decimal('longitude', 9, 6)->nullable();
+            $table->string('phone_num')->nullable();
+            $table->string('fax_num')->nullable();
+            $table->string('tax_id')->nullable();
+            $table->string('status')->nullable();
+            $table->string('is_default')->nullable();
+            $table->string('frontweb')->nullable();
+            $table->string('image_filename')->nullable();
+            $table->string('remarks')->nullable();
+            $table->string('date_format')->nullable();
+            $table->string('time_format')->nullable();
+            $table->string('thousand_separator')->nullable();
+            $table->string('decimal_separator')->nullable();
+            $table->integer('decimal_digit')->default(0);
+            $table->string('ribbon')->nullable();
+            $table->unsignedBigInteger('created_by')->default(0);
+            $table->unsignedBigInteger('updated_by')->default(0);
+            $table->unsignedBigInteger('deleted_by')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('companies');
+    }
+}
