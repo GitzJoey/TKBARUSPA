dde50bf667b4bb8c929fbb7b31a7dd4b0b9703d0
GitzJoey <gitzjoey@live.com>
2018-03-25 23:40:30 +0700

    prepare for register and login controller

---
+++ resources/lang/en/login.php
@@ -0,0 +1,28 @@
+<?php 
+
+return [
+    'email' => 'Email',
+    'password' => 'Password',
+    'title' => 'Sign In To Start',
+    'remember_me' => 'Remember Me',
+    'forgot_pass' => 'Forgot Password',
+    'register' => [
+        'new' => 'Sign Up',
+        'title' => 'Sign Up',
+        'company_name' => 'Company name',
+        'full_name' => 'Full name',
+        'email' => 'Email',
+        'password' => 'Password',
+        'retype_password' => 'Retype password',
+        'agree_1' => 'I agree to the ',
+        'agree_2' => 'terms',
+        'already_member' => 'I already have a membership',
+        'terms_and_cond' => 'Terms & Condition',
+    ],
+    'forgot' => [
+        'title' => 'Enter Your Email Address',
+    ],
+    'reset' => [
+        'title' => 'Enter Your New Password',
+    ]
+];
---
+++ resources/lang/id/login.php
@@ -0,0 +1,26 @@
+<?php 
+
+return [
+    'title' => 'Silahkan Login Untuk Memulai',
+    'remember_me' => 'Ingat Aku',
+    'forgot_pass' => 'Lupa Password',
+    'register' => [
+        'new' => 'Daftar Baru',
+        'title' => 'Daftar Baru',
+        'company_name' => 'Nama Perusahaan',
+        'full_name' => 'Nama Lengkap',
+        'email' => 'Email',
+        'password' => 'Password',
+        'retype_password' => 'Ulangi Password',
+        'agree_1' => 'Saya Menyetujui ',
+        'agree_2' => 'Syarat Dan Ketentuan',
+        'already_member' => 'Sudah Memiliki Akun',
+        'terms_and_cond' => 'Syarat Dan Ketentuan',
+    ],
+    'forgot' => [
+        'title' => 'Masukan Alamat Email Anda',
+    ],
+    'reset' => [
+        'title' => 'Masukan Password Baru Anda',
+    ]
+];
