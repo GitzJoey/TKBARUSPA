c13138caa588f4d3af691833ba0b3ce8eacad2ec
GitzJoey <gitzjoey@live.com>
2018-03-25 23:32:54 +0700

    install and settings some dependencies

---
+++ app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Laratrust\Models\LaratrustPermission;
+
+/**
+ * App\Models\Permission
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Role[] $roles
+ * @mixin \Eloquent
+ */
+class Permission extends LaratrustPermission
+{
+    //
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Laratrust\Models\LaratrustRole;
+
+/**
+ * App\Models\Role
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Permission[] $permissions
+ * @mixin \Eloquent
+ */
+class Role extends LaratrustRole
+{
+    //
+}
