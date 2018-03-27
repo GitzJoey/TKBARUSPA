f0726d635975be6780601f931acc1aa3ba1e84c1
GitzJoey <gitzjoey@live.com>
2018-03-28 00:22:53 +0700

    remove passport vue

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -64,9 +64,6 @@
         </div>
     </div>
     <br>
-    <passport-clients></passport-clients>
-    <passport-authorized-clients></passport-authorized-clients>
-    <passport-personal-access-tokens></passport-personal-access-tokens>
 @endsection
 
 @section('custom_js')
